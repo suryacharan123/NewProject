34816bbefec9dd7a95a83741e70c61de8d350546
Mohancherukuri <91059311+Mohancherukuri@users.noreply.github.com>
2024-03-01 13:46:31 +0530

    Delete src directory

--- src/Components/BookImage/BookImage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function BookImage({ image, className, style }) {
-  return (
-    <img src={image} alt='product' className={className} style={style} />
-  )
-}
-
-export default BookImage
--- src/Components/Buttons/OrangeButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function OrangeButton({ onClick, text, className }) {
-    return (
-        <button onClick={onClick} className={className}>{text}</button>
-    )
-}
-
-export default OrangeButton
